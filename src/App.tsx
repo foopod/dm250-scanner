598e7836aec0ea7a439f2f3901bff49b9f0ae49f
foopod <jonathonshields@gmail.com>
2025-03-15 09:24:36 +1300

    add result

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import { useState } from 'react'
 import './App.css'
 // import Scanner from './components/Scanner'
-import { Scanner } from '@yudiel/react-qr-scanner';
+import { IDetectedBarcode, Scanner } from '@yudiel/react-qr-scanner';
 
 function App() {
   const [scanning, setScanning] = useState(false)
   const [text, setText] = useState('')
+
+  const handleScan = (result: IDetectedBarcode[]) => {
+    result.forEach(r => {
+      setText((prev) => prev + r.rawValue)
+    })
+    setScanning(false)
+  }
 
   return (
     <>
       <button onClick={() => navigator.clipboard.writeText(text)} > Done </button>
       <button onClick={() => setScanning(true)} > Scan </button>
       { scanning &&
-        <Scanner onScan={(result) => setText(old => old + result)} />
+        <Scanner onScan={handleScan} />
       }
     </>
   )
