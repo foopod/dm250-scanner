8bddd1753ac1dca8d6cd3399dd73c144f0629680
foopod <jonathonshields@gmail.com>
2025-03-15 09:31:50 +1300

    Add text better

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,20 @@ function App() {
 
   const handleScan = (result: IDetectedBarcode[]) => {
     result.forEach(r => {
-      setText((prev) => prev + r.rawValue)
+      setText((prev) => prev + r.rawValue.slice(7))
     })
     setScanning(false)
   }
 
   return (
     <>
-      <button onClick={() => navigator.clipboard.writeText(text)} > Done </button>
-      <button onClick={() => setScanning(true)} > Scan </button>
-      { scanning &&
+      {text.length > 0 &&
+        <button className='mx-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400'
+          onClick={() => navigator.clipboard.writeText(text)} > Done </button>
+      }
+      <button className='mx-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400'
+        onClick={() => setScanning(true)} > Scan </button>
+      {scanning &&
         <Scanner onScan={handleScan} />
       }
     </>
