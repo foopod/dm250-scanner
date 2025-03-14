f496789f9c2750664ab56eb5cea9f58c35e40278
foopod <jonathonshields@gmail.com>
2025-03-15 09:37:25 +1300

    improvements

--- src/App.tsx
+++ src/App.tsx
@@ -18,12 +18,15 @@ function App() {
     <>
       {text.length > 0 &&
         <button className='mx-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400'
-          onClick={() => navigator.clipboard.writeText(text)} > Done </button>
+          onClick={() => navigator.clipboard.writeText(text)} > Copy to Clipboard </button>
       }
       <button className='mx-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400'
-        onClick={() => setScanning(true)} > Scan </button>
+        onClick={() => setScanning(true)} > {text.length > 0 ? 'Scan More' : 'Scan'} </button>
       {scanning &&
         <Scanner onScan={handleScan} />
+      }
+      {text.length > 0 &&
+        <p>{text}</p>
       }
     </>
   )
