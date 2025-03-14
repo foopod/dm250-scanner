c2b18f5869dbf51a2c08761308aa33092f689bab
foopod <jonathonshields@gmail.com>
2025-03-15 09:48:04 +1300

    show copy

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { IDetectedBarcode, Scanner } from '@yudiel/react-qr-scanner';
 function App() {
   const [scanning, setScanning] = useState(false)
   const [text, setText] = useState('')
+  const [preview, setPreview] = useState(false)
+  const [showCopied, setShowCopied] = useState(false)
 
   const handleScan = (result: IDetectedBarcode[]) => {
     result.forEach(r => {
@@ -14,24 +16,29 @@ function App() {
     setScanning(false)
   }
 
+  const copy = () => {
+    navigator.clipboard.writeText(text)
+    setShowCopied(true)
+    setTimeout(() => setShowCopied(false), 500)
+  }
+
   return (
     <>
       {text.length > 0 &&
         <>
-          <button className='mx-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400'
-            onClick={() => navigator.clipboard.writeText(text)} > Copy to Clipboard </button>
-          <button className='mx-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400'
+          <button className='mx-2 my-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400'
+            onClick={copy} > {showCopied ? 'Copied' : 'Copy'}</button>
+          <button className='mx-2 my-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400'
             onClick={() => setText('')} > Clear </button>
         </>
       }
-      <button className='mx-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400'
+      <button className='mx-2 my-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400'
         onClick={() => setScanning(true)} > {text.length > 0 ? 'Scan More' : 'Scan'} </button>
       {scanning &&
         <Scanner onScan={handleScan} components={{audio: false}}/>
       }
       {text.length > 0 &&
         <>
-
           <p>{text.length} characters</p>
           {/* <button className='mx-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400'
         onClick={() => setScanning(true)} > {text.length > 0 ? 'Preview'} </button> */}
